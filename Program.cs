aae433443a0d05cf92cfa80613412918fac33b9a
Mansour-Aldundur <mansour.aldundur@tuwaiq.edu.sa>
2021-04-18 20:26:27 +0300

    add: assignment files

---
+++ Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+
+namespace HelloCS
+{
+    class Program
+    {
+        class Searchable  {
+            string value ; 
+            public Searchable(string val){
+                this.value = val ; 
+            }
+
+            public int numOfwords() {
+                return this.value.Split(' ').Length ; 
+            }
+
+            public int numOfChars(){   
+                return this.value.Length ; 
+            }
+
+            public int numOfXword (string word) {
+                int counter = 0 ;
+                string [] result = this.value.Split(' '); 
+                foreach (var item in result)
+                {
+                    if (item ==word){
+                        counter++; 
+                    }
+                }
+                return counter ;
+            }
+
+            public int numOfXchars (char c) {
+                 int counter = 0 ;
+                 foreach(var item in this.value){
+                     if(c == item){
+                         counter++; 
+                     }
+                 }
+                return counter ; 
+            }
+
+            public int lastIndexOf(char c) {
+                int indexOfC = -1 ;
+                for(int i = 0 ; i < this.value.Length ; i++) {
+                    if(this.value[i]==c){
+                        indexOfC = i ; 
+                    }
+                }
+                return indexOfC ; 
+            }
+
+
+            public string mostWord() { 
+                
+                string [] toBeChecked = this.value.Split(' ') ; 
+                int counter = 0 ;
+                int mostCounter = 0 ; 
+                string mostString = "" ; 
+
+                foreach (var word in toBeChecked)
+                {
+                    for (int i = 0 ; i < toBeChecked.Length ; i++) {
+                        if(word == toBeChecked[i]){
+                            counter++ ; 
+                        }
+                        
+                    }
+                    if(counter > mostCounter){
+                        mostCounter = counter ; 
+                        mostString = word ; 
+                    }
+                counter =0 ; 
+                }
+                
+                
+                return mostString ;
+            }
+            // swap words 
+            public string SwapConsecativeWords(){
+                
+                string [] words = this.value.Split(' ') ;
+                string swappedString = "" ; 
+                
+                for(int i = 0 ; i < words.Length; i+=2){
+                    if(i == words.Length -1){
+                    swappedString += words[i] ; 
+                    break;
+                    }
+                    swappedString+= words[i+1] ; 
+                    swappedString+=" "+ words[i] +" " ; 
+                }
+
+                return swappedString; 
+            }
+
+
+
+        
+        }
+        static void Main(string[] args)
+        {
+            Searchable mn = new Searchable("ssss") ; // Write your tests here 
+            Console.WriteLine(mn.numOfwords());
+            Console.WriteLine(mn.numOfChars());
+            Console.WriteLine(mn.numOfXchars('s'));
+            Console.WriteLine(mn.numOfXword(""));
+            Console.WriteLine(mn.lastIndexOf('s'));
+            Console.WriteLine(mn.mostWord()); // finds the most repeated word in a String
+            Console.WriteLine(mn.SwapConsecativeWords());
+           
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+# Searchable 
+
+--- 
+
+An Implementation of a class to practice packaging of vars and methods and how to group them based on a relationship. 
+
+## notes 
+1- Deciding method interfaces is the most important thing before starting the Implementation 
+
+2- Programming is simply a **Data Processing**
+
+3- always think the big picture and set a reference point which is :
+    INPUTS -> Process -> OUTPUT 
